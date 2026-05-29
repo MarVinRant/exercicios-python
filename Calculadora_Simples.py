f97c12da59972e13097362ffaef3d83718591f3e
Marcos Rantigueri <marcos.seuemail@gmail.com>
2026-05-28 22:18:07 -0300

    Primeiro commit da calculadora

---
+++ Calculadora_Simples.py
@@ -0,0 +1,23 @@
+# Mini-Projeto: Calculadora Simples
+
+print("=== Calculadora Simples ===")
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+operacao = input("Digite a operação (+, -, *, /): ")
+
+if operacao == "+":
+    resultado = num1 + num2
+elif operacao == "-":
+    resultado = num1 - num2
+elif operacao == "*":
+    resultado = num1 * num2
+elif operacao == "/":
+    if num2 != 0:
+        resultado = num1 / num2
+    else:
+        resultado = "Erro: Divisão por zero!"
+else:
+    resultado = "Operação inválida!"
+
+print(f"\nResultado: {resultado}")
