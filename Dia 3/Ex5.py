a1088c82bf5075e43b6fa6715f52a3fc15cde572
Marcos Rantigueri <marcos.seuemail@gmail.com>
2026-06-02 09:45:36 -0300

    Ano bissexto

---
+++ Dia 3/Ex5.py
@@ -0,0 +1,12 @@
+# Faça um programa que leia um ano qualquer e mostre se ele é Bissexto.
+
+from datetime import date
+
+ano = int(input('Digite um ano: '))
+
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print(f'\033[1;32mO ano {ano} é bissexto.\033[m')
+else:
+    print(f'\033[1;31mO ano {ano} não é bissexto.\033[m')
