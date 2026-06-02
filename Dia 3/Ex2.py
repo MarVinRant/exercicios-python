821e73384e0813573d5b60e6cca9a1670eae22b2
Marcos Rantigueri <marcos.seuemail@gmail.com>
2026-06-02 09:02:37 -0300

    Radar de velocidade 80km

---
+++ Dia 3/Ex2.py
@@ -0,0 +1,11 @@
+# Escreva um programa que leia a velocidade de um carro.
+# Se ele ultrapassar 80km/h, mostre uma mensagem dizendo que ele foi multado.  
+# A multa vai custar R$7,00 por cada km acima do limite.
+
+velocidade = float(input('Digite a velocidade do carro: '))
+
+if velocidade > 80:
+    multa = (velocidade - 80) * 7
+    print(f'Você foi multado no valor de R${multa:.2f}.')
+else:
+    print('Velocidade dentro do limite. Boa viagem!')
