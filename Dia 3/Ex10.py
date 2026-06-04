d672f3e822804e20b8a87781c01993272eca3dbe
Marcos Rantigueri <marcos.seuemail@gmail.com>
2026-06-04 07:44:52 -0300

    Emprestimo de casa

---
+++ Dia 3/Ex10.py
@@ -0,0 +1,15 @@
+# Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
+# Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o empréstimo será negado.
+
+
+casa = float(input('Valor da casa: R$ '))
+salario = float(input('Salário do comprador: R$ '))
+anos = int(input('Quantos anos de financiamento? '))
+
+prestacao = casa / (anos * 12)
+minimo = salario * 0.3
+
+if prestacao <= minimo:
+    print('\033[32mEmprestimo APROVADO!\033[m')
+else:
+    print('\033[31mEmprestimo NEGADDO!\033[m')
